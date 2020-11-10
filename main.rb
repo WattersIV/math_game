1f6b66a26097b1f230b495d279d1baf61523e9fb
Bill Watters <billywatters13@gmail.com>
2020-11-10 01:53:06 +0000

    Add custom player names

--- main.rb
+++ main.rb
@@ -1,4 +1,10 @@
-require "game" 
-require "questiom" 
-require "player" 
+require './player' 
+require './game'
+require './question'
 
+a = Player.new('Player 1') 
+b = Player.new('Player 2') 
+puts "Player 1 name:"
+a.name = gets.chomp 
+puts "Player 2 name:"
+b.name = gets.chomp
